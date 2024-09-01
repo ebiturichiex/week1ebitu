94f36cc10eeeb9574ca5bd74173401d9128693f3
richie <ebiturichie2003@gmail.com>
2024-09-01 20:50:14 +0100

    my commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+public class twosums{
+    twosums(int[] nums,int target){
+        public static void main} (String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] result =
+         twosums (nums,target);
+        if (result.length ==2){
+        System.out.printf("indices: "+ result[0]+ "," + result[1]);
+
+
+
+
+
+        }
+    }
+}
